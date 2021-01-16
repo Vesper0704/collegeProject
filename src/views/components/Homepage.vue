ae9ae0f769eae7141631978511ac5dc9c03b8379
apple <2017213116@bupt.edu.cn>
2021-01-16 12:54:30 +0800

    commit test

--- src/views/components/Homepage.vue
+++ src/views/components/Homepage.vue
@@ -26,7 +26,7 @@
                 <div class="row justify-content-center align-items-center">
                     <div class="col-lg-7 text-center pt-lg">
 
-                      <p class="lead text-white-50 mt-4 mb-5 fontstyle" style="font-size:25px">Copyright Protection Platform based-on BLOCK CHAIN</p>
+                      <p class="lead text-white-50 mt-4 mb-5 fontstyle" style="font-size:30px">Copyright Protection Platform based-on BLOCK CHAIN</p>
                         <img src="img/brand/blockchain1.png" style="width: 2000px" class="img-fluid">
 
                       <p class="lead text-white-50 mt-4 mb-5 ">Thanks for choosing our platform  </p>
